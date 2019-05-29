9f1df8c1ac113e7ba412e71a7e1bde8e4d7dedf7
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-05-29 17:36:26 +0700

    update readme and config

--- src/components/PaymentButton.js
+++ src/components/PaymentButton.js
@@ -1,12 +1,18 @@
 import React, { Fragment, useEffect, useState } from "react";
 import canUseDOM from "can-use-dom";
 
+import {
+  methodData,
+  paymentInvoices,
+  paymentOptions
+} from "../mocks/config-payments";
+
 function PaymentButton(props) {
   const [isSupportWebPayment, setSupportWebPayment] = useState(true);
 
   useEffect(() => {
     if (window.PaymentRequest) {
-      console.log('[Browser Supported for Web Payment]...');
+      console.log("[Browser Supported for Web Payment]...");
       setSupportWebPayment(true);
     } else {
       console.log("[Browser Unsuported for Web Payment]...");
@@ -17,69 +23,39 @@ function PaymentButton(props) {
   const payNow = () => {
     if (canUseDOM) {
       if (window.PaymentRequest) {
-        const supportedPaymentMethods = [
-          {
-            supportedMethods: "basic-card",
-            data: {
-              supportedNetworks: ["visa", "mastercard", "amex"]
-            }
-          }
-        ];
-        const paymentDetails = {
-          displayItems: [
-            {
-              label: "Anvil L/S Crew Neck - Grey M x1",
-              amount: { currency: "USD", value: "22.15" }
-            }
-          ],
-          total: {
-            label: "Total due",
-            amount: { currency: "USD", value: "22.15" }
-          }
-        };
-
-        const options = {
-          requestShipping: true,
-          requestPayerEmail: true,
-          requestPayerPhone: true,
-          requestPayerName: true,
-          shippingType: "delivery"
-        };
-
-        // PaymentRequest Constructor
         const request = new PaymentRequest(
-          supportedPaymentMethods,
-          paymentDetails,
-          options
+          methodData,
+          paymentInvoices,
+          paymentOptions
         );
 
         request
           .show()
           .then(res => {
-            console.log("[PaymentRequest] ", res);
             res.complete("success");
           })
           .catch(err => {
             console.log(err);
           });
       } else {
-        console.log("[Unsupported] Browser not Support Payment Request..");
         console.log("[Unsupported] Please do traditional payment");
       }
-    } else {
-      console.log("Cannot use DOM");
     }
   };
 
   if (!isSupportWebPayment) {
     return (
-      <span className="not-support-webpayment">Your browser not supported Web Request Payment</span>
-    )
+      <span className="not-support-webpayment">
+        Your browser not supported Web Request Payment
+      </span>
+    );
   }
 
   return (
     <Fragment>
-      <button className="btn" onClick={payNow}> Web Payment Demo </button>
+      <button className="btn" onClick={payNow}>
+        Web Payment Demo
+      </button>
     </Fragment>
   );
 }
---
+++ src/mocks/config-payments.js
@@ -0,0 +1,27 @@
+export const methodData = [
+  {
+    supportedMethods: "basic-card",
+    data: {
+      supportedNetworks: ["visa", "mastercard"]
+    }
+  }
+];
+export const paymentInvoices = {
+  displayItems: [
+    {
+      label:
+        "SEPATU KERJA KANTOR CASUAL PANTOFEL BOOTS FORMAL KULIT PRIA NAVY SP2",
+      amount: { currency: "IDR", value: "134.900" }
+    }
+  ],
+  total: {
+    label: "Total Harga",
+    amount: { currency: "IDR", value: "134.900" }
+  }
+};
+export const paymentOptions = {
+  requestPayerEmail: true,
+  requestPayerPhone: true,
+  requestPayerName: true,
+  shippingType: "shipping"
+};
